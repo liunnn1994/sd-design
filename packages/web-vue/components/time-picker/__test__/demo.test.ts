60f85bb406f9343f6bc6c1aba86cdcbf13049be7
Norris Lau <liunian@2077tech.com>
2026-06-15 15:02:24 +0800

    test: 🧪 修复测试用例

--- packages/web-vue/components/time-picker/__test__/demo.test.ts
+++ packages/web-vue/components/time-picker/__test__/demo.test.ts
@@ -1,3 +1,14 @@
+import { afterEach, beforeEach, vi } from 'vitest';
+
 import demoTest from '../../../scripts/demo-test';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(new Date('2026-01-15T00:00:00'));
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 demoTest('time-picker');
